5d0846850f6241e8ae2a3cb49adce137734e3ec8
Simone Scalabrino <s.scalabrino9@gmail.com>
2017-10-06 11:28:08 +0200

    Fix on the structure checker

--- checkers/StructureChecker.rb
+++ checkers/StructureChecker.rb
@@ -13,7 +13,7 @@ module SCORM::Checkers
             
             error! "The first element must be a title page!" unless elements[0].name == "title"
             error! "The second element must be a connector menu!" unless elements[1].name == "connectorMenu"
-            error! "The second element must be a connector menu!" unless elements[-1].name == "accNav"
+            warning! "The last element should be a glossary" unless elements[-1].name == "accNav"
             
             for i in 2...elements.size-1
                 title = elements[i].xpath("@name").first.content
